e48bdd083b85d229ab7132c9e3507c76823272ae
Esta Nagy <nagyesta@gmail.com>
2023-06-25 16:54:14 +0200

    Update Abort-Mission versions in test modules - Updates Abort-Mission version in test modules
    {patch}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- gradle-tests/minimal-kts-off/build.gradle.kts
+++ gradle-tests/minimal-kts-off/build.gradle.kts
@@ -8,7 +8,7 @@ version = "0.0.1"
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("com.github.nagyesta.abort-mission.boosters:abort.booster-junit-jupiter:3.3.0")
+    testImplementation("com.github.nagyesta.abort-mission.boosters:abort.booster-junit-jupiter:4.0.0")
 }
 
 tasks.withType<Test> {
