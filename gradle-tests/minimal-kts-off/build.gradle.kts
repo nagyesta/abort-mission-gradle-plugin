833c32257d97511fec3c10033750314526772b09
Esta Nagy <nagyesta@gmail.com>
2023-08-11 23:23:52 +0200

    Maintenance - Prepares for new Gradle version
    {patch}

--- gradle-tests/minimal-kts-off/build.gradle.kts
+++ gradle-tests/minimal-kts-off/build.gradle.kts
@@ -9,6 +9,7 @@ version = "0.0.1"
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testImplementation("com.github.nagyesta.abort-mission.boosters:abort.booster-junit-jupiter:4.0.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
