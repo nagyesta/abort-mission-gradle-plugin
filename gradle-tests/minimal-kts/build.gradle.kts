2967e6d53172c47806c6bf7687edee7f96572e07
Esta Nagy <nagyesta@gmail.com>
2024-01-01 23:44:59 +0100

    Upgrade to JDK 17 - Upgrades JDK versions to 17 in all workflows - Prepares Abort Mission version updates (to use the JDK 17 Abort Mission version) - Stops using the deprecated buildDir Gradle reference
    {major}
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- gradle-tests/minimal-kts/build.gradle.kts
+++ gradle-tests/minimal-kts/build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1"
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("com.github.nagyesta.abort-mission.boosters:abort.booster-junit-jupiter:4.0.0")
+    testImplementation("com.github.nagyesta.abort-mission.boosters:abort.booster-junit-jupiter:5.0.0")
 }
 
 tasks.withType<Test> {
